9e3f9664d8f41f07d2480c61d714a19cfcfe8f5b
sto <Son.C.To@gmail.com>
2014-03-22 16:14:32 -0400

    type erasers are so annoying!

--- scalaAndroid/src/main/scala/com/datayumyum/pos/GridViewActivity.scala
+++ scalaAndroid/src/main/scala/com/datayumyum/pos/GridViewActivity.scala
@@ -12,11 +12,11 @@ class GridViewActivity extends Activity {
 
   override def onCreate(savedInstanceState: Bundle): Unit = {
     super.onCreate(savedInstanceState)
-    def parseJson(): Map[String, List[Map[String, Tuple2[String, Any]]]] = {
+    def parseJson(): Map[String, List[Map[String, Any]]] = {
       val jsonStr: String = Source.fromInputStream(getResources.openRawResource(R.raw.catalog)).mkString
       val result: Option[Any] = JSON.parseFull(jsonStr)
       result match {
-        case Some(r: Map[String, List[Map[String, Tuple2[String, Any]]]]) => {
+        case Some(r: Map[String, List[Map[String, Any]]]) => {
           return r
         }
         case Some(_) => {
@@ -29,9 +29,22 @@ class GridViewActivity extends Activity {
       }
     }
 
-    val catalog: Map[String, Any] = parseJson()
+    val catalog = parseJson()
+    val entrees = catalog("Entrees")
+    Log.i(TAG, entrees.toString())
+    entrees.foreach(item => {
+      val name: String = item("name") match {
+        case i: String => i
+      }
+      val url: String = item("imageURL") match {
+        case i: String => i
+      }
+      val price = item("price") match {
+        case d: Double => d
+      }
+      Log.i(TAG, name + " " + price.toString)
+    })
 
-    Log.i(TAG, catalog.toString)
     setContentView(R.layout.activity_grid_view)
   }
 }
