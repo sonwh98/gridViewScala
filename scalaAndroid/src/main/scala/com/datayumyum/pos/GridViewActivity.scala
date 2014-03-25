1a73570bdbf820626a5acd5b3e06690cc25b540f
sto <Son.C.To@gmail.com>
2014-03-25 15:17:47 -0400

    refactor adapter code to use implicits

--- scalaAndroid/src/main/scala/com/datayumyum/pos/GridViewActivity.scala
+++ scalaAndroid/src/main/scala/com/datayumyum/pos/GridViewActivity.scala
@@ -3,7 +3,6 @@ package com.datayumyum.pos
 import android.os.Bundle
 import android.app.Activity
 import scala.io.Source
-import android.util.Log
 import android.view.{ViewGroup, View, LayoutInflater}
 import android.widget.{TextView, ImageButton, BaseAdapter, GridView}
 
@@ -17,40 +16,43 @@ class GridViewActivity extends Activity {
     val jsonStr: String = Source.fromInputStream(getResources.openRawResource(R.raw.catalog)).mkString
     val catalog = Catalog.from(jsonStr)
     val entrees: List[Item] = catalog.findItemsByCategory("Entrees")
-
-    val inflater: LayoutInflater = LayoutInflater.from(this)
-    val itemButtonList = entrees.map(item => {
-      val itemButton: View = inflater.inflate(R.layout.item_button, null)
-      val imageButton: ImageButton = itemButton.findViewById(R.id.item_image_button).asInstanceOf[ImageButton]
-      new DownloadImageTask(imageButton).execute(item.imageURL)
-
-      val itemLabel: TextView = itemButton.findViewById(R.id.item_label).asInstanceOf[TextView]
-      itemLabel.setText(item.name)
-      itemButton
-    })
-
     val gridView: GridView = findViewById(R.id.gridview).asInstanceOf[GridView]
-    gridView.setAdapter(new BaseAdapter() {
-      @Override def getCount: Int = {
-        return itemButtonList.size
-      }
-
-      @Override def getItem(position: Int): Object = {
-        return null
-      }
-
-      @Override def getItemId(position: Int): Long = {
-        return 0
-      }
-
-      @Override def getView(position: Int, convertView: View, parent: ViewGroup): View = {
-        val imageView: View = itemButtonList(position)
-        return imageView
-      }
-    })
-
-    Log.i(TAG, gridView.toString)
-
+    gridView.setAdapter(entrees)
   }
+
+  implicit class GridAdapter(items: List[Item]) extends BaseAdapter {
+    var itemButtonList: List[View] = List()
+
+    @Override def getCount: Int = {
+      return items.size
+    }
+
+    @Override def getItem(position: Int): Object = {
+      return null
+    }
+
+    @Override def getItemId(position: Int): Long = {
+      return 0
+    }
+
+    @Override def getView(position: Int, convertView: View, parent: ViewGroup): View = {
+      if (itemButtonList.size == 0) {
+        val inflater: LayoutInflater = LayoutInflater.from(GridViewActivity.this)
+        itemButtonList = items.map(item => {
+          val itemButton: View = inflater.inflate(R.layout.item_button, null)
+          val imageButton: ImageButton = itemButton.findViewById(R.id.item_image_button).asInstanceOf[ImageButton]
+          new DownloadImageTask(imageButton).execute(item.imageURL)
+
+          val itemLabel: TextView = itemButton.findViewById(R.id.item_label).asInstanceOf[TextView]
+          itemLabel.setText(item.name)
+          itemButton
+        })
+      }
+
+      val imageView: View = itemButtonList(position)
+      return imageView
+    }
+  }
+
 }
 
