9c7615d02b922af465c9eb42103803f813bb53f0
sto <Son.C.To@gmail.com>
2014-03-26 16:08:17 -0400

    refactor. remove pattern matching

--- scalaAndroid/src/main/scala/com/datayumyum/pos/Catalog.scala
+++ scalaAndroid/src/main/scala/com/datayumyum/pos/Catalog.scala
@@ -1,30 +1,25 @@
 package com.datayumyum.pos
 
 import scala.util.parsing.json.JSON
-import scala.collection.mutable.HashMap
+import scala.collection.mutable
 
 object Catalog {
   def from(jsonStr: String): Catalog = {
     val result: Option[Any] = JSON.parseFull(jsonStr)
-    val menu: HashMap[String, List[Item]] = result match {
-      case Some(parsedMap: Map[String, List[Map[String, Any]]]) => {
-        val menuMap = HashMap.empty[String, List[Item]]
-        for ((category, mapList) <- parsedMap) {
-          val itemList = mapList.map(m => {
-            Item(m("name").asInstanceOf[String], m("imageURL").asInstanceOf[String], m("price").asInstanceOf[Double])
-          })
-          menuMap(category) = itemList
-        }
-        menuMap
-      }
-      case _ => throw new RuntimeException("cannot parse json " + jsonStr)
+    val parsedMap: Map[String, List[Map[String, Any]]] = result.get.asInstanceOf[Map[String, List[Map[String, Any]]]]
+    val menu: mutable.HashMap[String, List[Item]] = mutable.HashMap.empty[String, List[Item]]
+    for ((category, mapList) <- parsedMap) {
+      val itemList = mapList.map(m => {
+        Item(m("name").asInstanceOf[String], m("imageURL").asInstanceOf[String], m("price").asInstanceOf[Double])
+      })
+      menu(category) = itemList
     }
 
     return new Catalog(menu)
   }
 }
 
-class Catalog(map: HashMap[String, List[Item]]) {
+class Catalog(map: mutable.HashMap[String, List[Item]]) {
   def findItemsByCategory(name: String): List[Item] = {
     map(name)
   }
